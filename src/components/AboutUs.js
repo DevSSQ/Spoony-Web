8cb25114f7dcd8c364ed29ddffb0af93ec59141a
Sara <sara@gmail.com>
2022-11-07 12:51:29 +0300

    Add Nav and About us

---
+++ src/components/AboutUs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div className='aboutus'>
+
+    <div>AboutUs</div>
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ src/components/Nav.js
@@ -0,0 +1,81 @@
+import { ReactNode } from 'react';
+import { React } from 'react';
+
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  Link,
+  IconButton,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon, AddIcon } from '@chakra-ui/icons';
+
+const Links = ['Home Page', 'Recipes', 'About Us'];
+
+const NavLink =( { children })=> (
+  <Link
+    px={2}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('#f0677c'),
+    }}
+    href={'./AboutUs'}>
+    {children}
+  </Link>
+);
+
+export default function Nav() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box color ='#F2F2F2'bg={useColorModeValue('#A7D6CE', 'gray.900')} px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+          
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack >
+            <Box><img className='spoonylogo' src="./Spoonylogo.png"/></Box>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+         
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+
+    </>
+  );
+}
