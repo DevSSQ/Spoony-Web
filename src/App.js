cd50c9ed29748d9ddd4c9fa5ec708c4722de9e5f
Sarahalnuu <Saraotb4400@gmail.com>
2022-11-08 15:11:11 +0300

    add login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import './App.css';
-<<<<<<< HEAD
 import Footer from './components/Footer';
-=======
 import AboutUs from './components/AboutUs';
 import Nav from './components/Nav';
 import HomePage from './components/HomePage';
->>>>>>> a2a1b8e3457a0ef1da44c40ce03ba4b56be652c7
+
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <Footer/>
-=======
 
-      <Nav></Nav>
-      <AboutUs></AboutUs>
-
-     <HomePage></HomePage>
->>>>>>> a2a1b8e3457a0ef1da44c40ce03ba4b56be652c7
 
     </div>
   );
