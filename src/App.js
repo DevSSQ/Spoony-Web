f398ddac905e2e8f229e37ca8aff13d8428dcae8
Sarah H <sara.s.qahtanii@gmail.com>
2022-11-08 14:53:47 +0300

    Recipe Card is done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import AboutUs from './components/AboutUs';
-import Nav from './components/Nav';
-import HomePage from './components/HomePage';
-import Recipes from './components/Recipes';
+
 
 function App() {
   return (
     <div className="App">
-  
-  <Recipes/>
-
 
 
     </div>
