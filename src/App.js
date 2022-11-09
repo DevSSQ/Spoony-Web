44d62658376fa8468f3b0de438778a94c9343030
Sarah H <sara.s.qahtanii@gmail.com>
2022-11-09 19:29:52 +0300

    nothing new

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import './App.css';
-<<<<<<< HEAD
-=======
- 
->>>>>>> origin
-import Footer from './components/Footer';
-import AboutUs from './components/AboutUs';
 import Nav from './components/Nav';
 import HomePage from './components/HomePage';
-<<<<<<< HEAD
-=======
-import FullRecipes from './components/FullRecipes';
-
-
->>>>>>> origin
-
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
+      <HomePage/>
+      <Footer/>
 
-<<<<<<< HEAD
-=======
-
->>>>>>> origin
 
     </div>
 
